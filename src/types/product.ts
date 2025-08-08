d4899c4a33669f8cba9fd6710361eb5e2266663c
AshwinGoutam <ashwanigoutam16@gmail.com>
2025-08-08 08:16:55 +0530

    latest code

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  title: string;
+  description: string;
+  reviews: number;
+  price: number;
+  discountedPrice: number;
+  id: number;
+  imgs?: {
+    thumbnails: string[];
+    previews: string[];
+  };
+};
