636589eaa96d2aa0b7d116a2e5f6f47855cc58a6
AshwinGoutam <ashwanigoutam16@gmail.com>
2025-08-23 17:50:08 +0530

    live jai shree ram

--- src/components/Shop/SingleListItem.tsx
+++ src/components/Shop/SingleListItem.tsx
@@ -30,21 +30,21 @@ const SingleListItem = ({ item }: { item: Product }) => {
     );
   };
 
-  const handleItemToWishList = () => {
-    dispatch(
-      addItemToWishlist({
-        ...item,
-        status: "available",
-        quantity: 1,
-      })
-    );
-  };
+  // const handleItemToWishList = () => {
+  //   dispatch(
+  //     addItemToWishlist({
+  //       ...item,
+  //       status: "available",
+  //       quantity: 1,
+  //     })
+  //   );
+  // };
 
   return (
     <div className="group rounded-lg bg-white shadow-1">
       <div className="flex">
         <div className="shadow-list relative overflow-hidden flex items-center justify-center max-w-[270px] w-full sm:min-h-[270px] p-4">
-          <Image src={item.imgs.previews[0]} alt="" width={250} height={250} />
+          <img src={item.image_url} alt="" width={250} height={250} />
 
           <div className="absolute left-0 bottom-0 translate-y-full w-full flex items-center justify-center gap-2.5 pb-5 ease-linear duration-200 group-hover:translate-y-0">
             <button
@@ -85,7 +85,7 @@ const SingleListItem = ({ item }: { item: Product }) => {
               Add to cart
             </button>
 
-            <button
+            {/* <button
               onClick={() => handleItemToWishList()}
               aria-label="button for favorite select"
               className="flex items-center justify-center w-9 h-9 rounded-[5px] shadow-1 ease-out duration-200 text-dark bg-white hover:text-blue"
@@ -105,19 +105,19 @@ const SingleListItem = ({ item }: { item: Product }) => {
                   fill=""
                 />
               </svg>
-            </button>
+            </button> */}
           </div>
         </div>
 
         <div className="w-full flex flex-col gap-5 sm:flex-row sm:items-center justify-center sm:justify-between py-5 px-4 sm:px-7.5 lg:pl-11 lg:pr-12">
           <div>
             <h3 className="font-medium text-dark ease-out duration-200 hover:text-blue mb-1.5">
-              <Link href="/shop-details"> {item.title} </Link>
+              <Link href="/shop-details"> {item.name} </Link>
             </h3>
 
             <span className="flex items-center gap-2 font-medium text-lg">
-              <span className="text-dark">${item.discountedPrice}</span>
-              <span className="text-dark-4 line-through">${item.price}</span>
+              <span className="text-dark">${item.price}</span>
+              <span className="text-dark-4 line-through">${item.mrp}</span>
             </span>
           </div>
 
@@ -155,7 +155,7 @@ const SingleListItem = ({ item }: { item: Product }) => {
               />
             </div>
 
-            <p className="text-custom-sm">({item.reviews})</p>
+            {/* <p className="text-custom-sm">({item.reviews})</p> */}
           </div>
         </div>
       </div>
